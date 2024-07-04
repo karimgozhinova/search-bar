6a4df60e3ad2259307bdee58f48518bf9d891df2
Aidana <karimgozhinova@gmail.com>
2024-07-05 00:48:52 +0500

    search-bar

---
+++ src/app/search-bar/search-bar.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-bar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './search-bar.component.html',
+  styleUrl: './search-bar.component.css'
+})
+export class SearchBarComponent {
+  isActive: boolean = false;
+
+  toggleSearch() {
+    this.isActive = !this.isActive;
+  }
+}
